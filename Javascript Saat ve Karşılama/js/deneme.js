7b4bd8bc764b897dae4c89003ee70236bf4b4321
Büşra Yeşilalioğlu <88002831+7Auri@users.noreply.github.com>
2021-11-06 22:27:44 +0300

    Update deneme.js

--- Javascript Saat ve Karşılama/js/deneme.js
+++ Javascript Saat ve Karşılama/js/deneme.js
@@ -5,42 +5,46 @@ function askName(){
 }
 
 function showTime(){
-    var date = new Date();
-    var h = date.getHours();
-    var m = date.getMinutes();
-    var s = date.getSeconds();
-    var sezon = date.getDay();
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  var sezon = date.getDay();
 
-if(sezon == 1){
-    sezon = "Pazartesi"
+  if(sezon == 1){
+    sezon = "Pazartesi";
 }
 else if( sezon == 2){
-    sezon = "Salı"
+    sezon = "Salı";
 }
 else if( sezon == 3){
-    sezon = "Çarşamba"
+    sezon = "Çarşamba";
 }
 else if( sezon == 4){
-    sezon = "Perşembe"
+    sezon = "Perşembe";
 }
 else if( sezon == 5){
-    sezon = "Cuma"
+    sezon = "Cuma";
 }
 else if( sezon == 6){
-    sezon = "Cumartesi"
+    sezon = "Cumartesi";
 }
 else{
-    sezon = "Pazar"
+    sezon = "Pazar";
 }   
 
 h = h < 10 ? "0" + h : h;
 m = m < 10 ? "0" + m : m;
 s = s < 10 ? "0" + s : s;
 
-var time = h + ":" + m + ":" + s
-
+var time = h + ":" + m + ":" + s + "" + sezon ;
+document.getElementById("myClock").innerHTML = time;
+setTimeout(showTime, 1000);
 
 }
+
+
+
 
 
 askName();
